e7cbe093f3a61c069c444d209c1e86eb087289ae
GokhanErtgn <gokhanertogan@gmail.com>
2021-02-28 00:38:03 +0300

    added new class

---
+++ ConsoleApp8/deneme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class deneme
+    {
+        public deneme()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
